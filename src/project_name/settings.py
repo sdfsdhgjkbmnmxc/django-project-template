4ad1017d9a8a8b26f4ea427f5341360c9e3bc719
Maxim Oransky <om@ostrovok.ru>
2013-01-13 20:31:39 +0400

    utils.log removed

--- src/project_name/settings.py
+++ src/project_name/settings.py
@@ -121,45 +121,31 @@ INTERNAL_IPS = (
 )
 
 
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'filters': {
         'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
-        },
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
     },
     'handlers': {
-        'stderr': {
-            'class': 'logging.StreamHandler',
-            'level': 'DEBUG' if DEBUG else 'NOTSET',
-        },
-        'logfile': {
-            'level': 'INFO',
-            'path': LOG_DIR,
-            'root_name': PROJ_NAME,
-            'filters': ['require_debug_false'],
-            'class': PROJ_NAME + '.utils.log.MultiFileHandler',
-        },
         'mail_admins': {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler',
-        },
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
     },
     'loggers': {
         'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
             'propagate': True,
         },
-        'django': {
-            'propagate': True,
-        },
-        'multiprocessing': {
-            'propagate': True,
-        },
-    },
-    'root': {
-        'handlers': ['logfile', 'mail_admins', 'stderr'],
-        'level': 'DEBUG' if DEBUG else 'INFO',
     }
 }
