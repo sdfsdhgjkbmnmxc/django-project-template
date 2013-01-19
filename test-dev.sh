25b138cdfacaeb38ce37f3ffa39a87a347ba9574
Maxim Oransky <om@ostrovok.ru>
2013-01-19 19:04:57 +0400

    flake8

--- test-dev.sh
+++ test-dev.sh
@@ -2,5 +2,6 @@
 set -e
 ROOT=`dirname "${BASH_SOURCE[0]}"`
 $ROOT/venv.sh pep8 $ROOT/src --show-source
+$ROOT/venv.sh flake8 $ROOT/src --show-source
 export DJANGO_SETTINGS_MODULE={{ project_name }}.testsettings
 $ROOT/venv.sh src/manage.py test $@
