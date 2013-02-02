d34593fc518f5b049daad3e815f293a98ed6e3dd
Maxim Oransky <om@ostrovok.ru>
2013-02-03 00:12:03 +0400

    pep8 removed

--- system/project_name.sql
+++
@@ -1,6 +0,0 @@
--- init
-CREATE DATABASE {{ project_name }};
-CREATE USER {{ project_name }};
-ALTER USER {{ project_name }} PASSWORD '{{ project_name }}';
-GRANT ALL ON DATABASE {{ project_name }} TO {{ project_name }};
-
--- test-dev.sh
+++ test-dev.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 ROOT=`dirname "${BASH_SOURCE[0]}"`
-$ROOT/venv.sh pep8 $ROOT/src --show-source
-$ROOT/venv.sh flake8 $ROOT/src --show-source
+
+echo "tests"
 export DJANGO_SETTINGS_MODULE={{ project_name }}.testsettings
 $ROOT/venv.sh src/manage.py test $@
+
+echo "code checks"
+$ROOT/venv.sh flake8 $ROOT/src --show-source
