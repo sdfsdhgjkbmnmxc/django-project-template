6ab9526c1bba84f87a23c9ea2b7bec5dee131d71
Maxim Oransky <om@ostrovok.ru>
2013-02-16 19:11:45 +0400

    venv fix

--- venv.sh
+++ venv.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-set -e
 ROOT=`dirname "${BASH_SOURCE[0]}"`
 act="$ROOT/system/venv/bin/activate"
 mkdir -p $ROOT/system/venv
 
 if [ ! -f "$act" ]; then
+    set -e
     pip install virtualenv
     virtualenv $ROOT/system/venv
     source $act
     $ROOT/upgrade-requirements.sh
+    set +e
 else
     source $act
 fi
