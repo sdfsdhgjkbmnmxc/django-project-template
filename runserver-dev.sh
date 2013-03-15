cd8e124ecdf2679e0d1ea56bd8b6140071215a45
Maxim Oransky <om@ostrovok.ru>
2013-03-16 00:33:46 +0400

    south fix

--- runserver-dev.sh
+++ runserver-dev.sh
@@ -4,7 +4,10 @@ ROOT=`dirname "${BASH_SOURCE[0]}"`
 NAME=`cat ${ROOT}/system/name.conf`
 export DJANGO_SETTINGS_MODULE=${NAME}.localsettings
 source ${ROOT}/venv.sh
-set +e; src/manage.py schemamigration ${NAME} --auto; set +e;
-src/manage.py migrate ${NAME}
-src/manage.py syncdb
-exec src/manage.py runserver
+if [ ! -d ${ROOT}/src/${NAME}/migrations ]; then
+    ${ROOT}/src/manage.py schemamigration ${NAME} --initial
+fi
+set +e; ${ROOT}/src/manage.py schemamigration ${NAME} --auto; set +e;
+${ROOT}/src/manage.py migrate ${NAME}
+${ROOT}/src/manage.py syncdb
+exec ${ROOT}/src/manage.py runserver
