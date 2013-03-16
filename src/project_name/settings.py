349211ab01af8fcb5442d882109d533f7e77b11d
Maxim Oransky <om@ostrovok.ru>
2013-03-16 04:30:20 +0400

    deploy fix

--- src/project_name/settings.py
+++ src/project_name/settings.py
@@ -16,6 +16,8 @@ else:
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+ALLOWED_HOSTS = ['*']
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
--- system/postinstall.sh
+++ system/postinstall.sh
@@ -2,40 +2,40 @@
 set -e
 ROOT=`dirname "${BASH_SOURCE[0]}"`
 
-NAME=`cat $ROOT/name.conf`
+NAME=`cat ${ROOT}/name.conf`
 
-PROJECT_ROOT=/var/www/$NAME
+PROJECT_ROOT=/var/www/${NAME}
 USER=www-data
 GROUP=www-data
 HOST=$1
 
 echo "Virtualenv..."
-$ROOT/venv.sh
-$ROOT/upgrade-requirements.sh
+${ROOT}/../venv.sh
+${ROOT}/../upgrade-requirements.sh
 
 echo "Logs..."
-mkdir -p /var/log/$NAME
-chown -R $USER:$GROUP /var/log/$NAME
-ln -sf $PROJECT_ROOT/system/logrotate.conf /etc/logrotate.d/$NAME
-chown root:root $PROJECT_ROOT/system/logrotate.conf
+mkdir -p /var/log/${NAME}
+chown -R ${USER}:${GROUP} /var/log/${NAME}
+ln -sf ${PROJECT_ROOT}/system/logrotate.conf /etc/logrotate.d/${NAME}
+chown root:root ${PROJECT_ROOT}/system/logrotate.conf
 
 echo "Supervisor..."
-ln -sf $PROJECT_ROOT/system/supervisor.conf /etc/supervisor/conf.d/$NAME.conf
+ln -sf ${PROJECT_ROOT}/system/supervisor.conf /etc/supervisor/conf.d/${NAME}.conf
 supervisorctl update
 supervisorctl restart ${NAME}_fcgi
 
 echo "Nginx..."
-ln -sf $PROJECT_ROOT/system/nginx.conf /etc/nginx/sites-enabled/$NAME
+ln -sf ${PROJECT_ROOT}/system/nginx.conf /etc/nginx/sites-enabled/${NAME}
 /etc/init.d/nginx reload
 
 echo "Static files..."
-$PROJECT_ROOT/venv.sh src/manage.py collectstatic --link --noinput
-chgrp -R www-data $PROJECT_ROOT/web/
-chmod -R 0775 $PROJECT_ROOT/web/
+${PROJECT_ROOT}/venv.sh src/manage.py collectstatic --link --noinput
+chgrp -R www-data ${PROJECT_ROOT}/web/
+chmod -R 0775 ${PROJECT_ROOT}/web/
 
 echo "Cron..."
-ln -sf $PROJECT_ROOT/system/cron.conf /etc/cron.d/$NAME
-chown root:root $PROJECT_ROOT/system/cron.conf
+ln -sf ${PROJECT_ROOT}/system/cron.conf /etc/cron.d/${NAME}
+chown root:root ${PROJECT_ROOT}/system/cron.conf
 
 echo "Create database"
-$PROJECT_ROOT/venv.sh src/manage.py syncdb --noinput
+${PROJECT_ROOT}/venv.sh src/manage.py syncdb --noinput
