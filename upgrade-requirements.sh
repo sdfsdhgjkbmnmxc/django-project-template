3bc5cad3b44db59e3c22c20861daee2518da0bda
Maxim Oransky <om@ostrovok.ru>
2013-04-02 04:41:23 +0400

    upgrade fix

--- upgrade-requirements.sh
+++ upgrade-requirements.sh
@@ -2,4 +2,4 @@
 set -e
 ROOT=`dirname "${BASH_SOURCE[0]}"`
 source ${ROOT}/venv.sh
-exec pip install -r system/requirements.txt --upgrade
+exec pip install -r ${ROOT}/system/requirements.txt --upgrade
