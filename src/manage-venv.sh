74871e7a0b1cca73c3e1e1295ad1be9134d8cde8
Maxim Oransky <om@ostrovok.ru>
2013-04-02 14:54:58 +0400

    psycopg

--- src/manage-venv.sh
+++ src/manage-venv.sh
@@ -2,5 +2,5 @@
 set -e
 SCRIPT_ROOT=`dirname "${BASH_SOURCE[0]}"`
 
-source ${SCRIPT_ROOT}/../system/venv.`hostname`/bin/activate
+source ${SCRIPT_ROOT}/../system/venv/bin/activate
 exec ${SCRIPT_ROOT}/manage.py $@
