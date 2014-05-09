c28044774feb4de2655aab18967aa150260303ed
Maxim Oransky <maxim.oransky@gmail.com>
2014-05-09 14:06:31 +0400

    mac os fixes

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -2,6 +2,5 @@
 ROOT=`dirname "${BASH_SOURCE[0]}"`
 
 export HOST=`cat ${ROOT}/system/host.conf`
-sed -i 's/YOUR\.DOMAIN/${HOST}/g' ${ROOT}/system/*
-
+#sed -i 's/YOUR\.DOMAIN/${HOST}/g' ${ROOT}/system/*
 ${ROOT}/system/_deploy-dev.sh
--- upgrade-requirements.sh
+++ upgrade-requirements.sh
@@ -2,4 +2,8 @@
 set -e
 ROOT=`dirname "${BASH_SOURCE[0]}"`
 source ${ROOT}/venv.sh
+if [[ "`uname`" == "Darwin" ]]; then
+    export CFLAGS=-Qunused-arguments
+    export CPPFLAGS=-Qunused-arguments
+fi
 exec pip install -r ${ROOT}/system/requirements.txt --upgrade
