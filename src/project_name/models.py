e112d43cb90e967e7c23bbffeeb9bd9b30b73db9
Maxim Oransky <maxim.oransky@gmail.com>
2014-08-24 15:11:04 +0400

    easy uncomment

--- src/project_name/models.py
+++ src/project_name/models.py
@@ -2,11 +2,11 @@
 from django.db import models
 
 
-#class MyModel(models.Model):
-#    def __unicode__(self):
-#        return u''
+# class MyModel(models.Model):
+#     def __unicode__(self):
+#         return u''
 #
-#    class Meta:
-#        verbose_name = u''
-#        verbose_name_plural = u''
-#        ordering = ('-id',)
+#     class Meta:
+#         verbose_name = u''
+#         verbose_name_plural = u''
+#         ordering = ('-id',)
