551652f1d5d79f3a588a235fcf733f4f278ccd54
Maxim Oransky <maxim.oransky@gmail.com>
2014-10-29 18:32:50 +0300

    django 1.7

--- system/postinstall.sh
+++ system/postinstall.sh
@@ -40,5 +40,4 @@ chown root:root ${PROJECT_ROOT}/system/cron.conf
 
 echo "Create database"
 source ${PROJECT_ROOT}/venv.sh
-${PROJECT_ROOT}/src/manage.py syncdb --noinput
-${PROJECT_ROOT}/src/manage.py migrate
+${PROJECT_ROOT}/src/manage.py migrate --noinput
