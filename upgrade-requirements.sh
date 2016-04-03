1274b44fc11be637601a8fcec94f95945de9dce5
Maxim Oransky <maxim.oransky@gmail.com>
2016-04-03 14:24:13 +0300

    mac os x requirements

--- upgrade-requirements.sh
+++ upgrade-requirements.sh
@@ -4,6 +4,9 @@ cd `dirname $0`
 if [[ "`uname`" == "Darwin" ]]; then
     export CFLAGS=-Qunused-arguments
     export CPPFLAGS=-Qunused-arguments
+    if [[ -n "`which brew`" ]]; then
+        brew install `cat brew-requirements.txt`
+    fi
 fi
 pyvenv .venv
 .venv/bin/pip install -r system/requirements.txt --upgrade
